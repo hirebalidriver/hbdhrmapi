d78e27e68b8e0b8798af07bc9b39696edcf07245
Andre Nata <andrenata25@gmail.com>
2023-02-26 21:41:36 +0800

    fix: collect

--- app/Http/Resources/TrxResource.php
+++ app/Http/Resources/TrxResource.php
@@ -34,6 +34,7 @@ class TrxResource extends JsonResource
             'guestName' => $this->booking->name,
             'hotel' => $this->booking->hotel,
             'guide_fee' => 'IDR '.number_format($this->booking->guide_fee, 0, '.', '.'),
+            'collect' => 'USD '.number_format($this->booking->collect, 0, '.', '.').' / '.'IDR '.number_format($this->booking->collect*15000, 0, '.', '.'),
             'susuk' => 'IDR '.number_format(($this->booking->susuk_guide+$this->booking->susuk_hbd), 0, '.', '.'),
             'cost' => 'IDR '.number_format($this->booking->tiket_total, 0, '.', '.'),
             'additional' => 'IDR '.number_format($this->booking->additional_price, 0, '.', '.'),

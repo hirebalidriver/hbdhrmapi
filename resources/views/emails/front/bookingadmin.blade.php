bcbdcf42b23be521c15793b458094fa22c34c39c
Andre Nata <andrenata25@gmail.com>
2023-05-07 09:41:24 +0800

    Update mail

---
+++ resources/views/emails/front/bookingadmin.blade.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en-US">
+   <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+      <title>Most Popular Tours in Bali – Hire Bali Driver</title>
+   </head>
+   <body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0" style="padding: 0;">
+      <div id="wrapper" dir="ltr" style="background-color: #f7f7f7; margin: 0; padding: 70px 0; width: 100%; -webkit-text-size-adjust: none;" bgcolor="#f7f7f7" width="100%">
+         <table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%">
+            <tr>
+               <td align="center" valign="top">
+
+                  <table border="0" cellpadding="0" cellspacing="0" width="600" id="template_container" style="background-color: #fff; border: 1px solid #dedede; box-shadow: 0 1px 4px rgba(0,0,0,.1); border-radius: 3px;" bgcolor="#fff">
+                     <tr>
+                        <td align="center" valign="top">
+                           <!-- Header -->
+                           <table border="0" cellpadding="0" cellspacing="0" width="100%" id="template_header" style='background-color: #077944; color: #fff; border-bottom: 0; font-weight: bold; line-height: 100%; vertical-align: middle; font-family: "Times New Roman", sans-serif; border-radius: 3px 3px 0 0;' bgcolor="#077944">
+                              <tr>
+                                 <td id="header_wrapper" style="padding: 36px 48px; display: block;">
+                                    <h1 style='font-family: "Times New Roman", sans-serif; font-size: 30px; font-weight: 300; line-height: 150%; margin: 0; text-align: left; text-shadow: 0 1px 0 #399469; color: #fff; background-color: inherit;' bgcolor="inherit">New Booking from Hirebalidriver.com</h1>
+                                 </td>
+                              </tr>
+                           </table>
+                           <!-- End Header -->
+                        </td>
+                     </tr>
+                     <tr>
+                        <td align="center" valign="top">
+                           <!-- Body -->
+                           <table border="0" cellpadding="0" cellspacing="0" width="600" id="template_body">
+                              <tr>
+                                 <td valign="top" id="body_content" style="background-color: #fff;" bgcolor="#fff">
+                                    <!-- Content -->
+                                    <table border="0" cellpadding="20" cellspacing="0" width="100%">
+                                       <tr>
+                                          <td valign="top" style="padding: 48px 48px 32px;">
+                                             <div id="body_content_inner" style='color: #636363; font-family: "Times New Roman", sans-serif; font-size: 14px; line-height: 150%; text-align: left;' align="left">
+                                                <p style="margin: 0 0 16px;">Hi Hirebalidriver,</p>
+
+                                                <h2 style='color: #077944; display: block; font-family: "Times New Roman", sans-serif; font-size: 18px; font-weight: bold; line-height: 130%; margin: 0 0 18px; text-align: left;'>
+                                                   Order #11018
+                                                </h2>
+                                                <h2 style='color: #2f3134; display: block; font-family: "Times New Roman", sans-serif; font-size: 18px; font-weight: bold; line-height: 130%; margin: 0 0 18px; text-align: left;'>
+                                                   Half Day Ubud Tour Ubud Unique Spots
+                                                </h2>
+                                                <p style="margin: 0 0 16px;"><strong>Option:</strong> Tour with Lunch</p>
+                                                <p style="margin: 0 0 16px;"><strong>Date:</strong> 01 January 2023</p>
+                                                <p style="margin: 0 0 16px;"><strong>Time:</strong> 8.00 AM</p>
+                                                 <p style="margin: 0 0 16px;"><strong>Name:</strong> Andre Nata</p>
+                                                 <p style="margin: 0 0 16px;"><strong>Adult:</strong> 2 x USD 59</p>
+                                                 <p style="margin: 0 0 16px;"><strong>Child:</strong> 2 x USD 35</p>
+                                                 <p style="margin: 0 0 16px;"><strong>Total:</strong> USD XXX</p>
+                                                 <p style="margin: 0 0 16px;"><strong>Email:</strong> buyer@gmail.com</p>
+                                                 <p style="margin: 0 0 16px;"><strong>Phone:</strong> Indonesia 281338123456</p>
+                                                <p style="margin: 0 0 16px;"><strong>Hotel:</strong> My Hotel</p>
+                                                <p style="margin: 0 0 16px;"><strong>Hotel address:</strong> Ubud</p>
+                                                <p style="margin: 0 0 16px;"><strong>Payment:</strong> Pay Now - Paypal (PAYPAL ID)</p>
+                                                <p style="margin: 0 0 16px;"><strong>Special requirements:</strong> Test order</p>
+
+                                             </div>
+                                          </td>
+                                       </tr>
+                                    </table>
+                                    <!-- End Content -->
+                                 </td>
+                              </tr>
+                           </table>
+                           <!-- End Body -->
+                        </td>
+                     </tr>
+                  </table>
+               </td>
+            </tr>
+            <tr>
+               <td align="center" valign="top">
+                  <!-- Footer -->
+                  <table border="0" cellpadding="10" cellspacing="0" width="600" id="template_footer">
+                     <tr>
+                        <td valign="top" style="padding: 0; border-radius: 6px;">
+                           <table border="0" cellpadding="10" cellspacing="0" width="100%">
+                              <tr>
+                                 <td colspan="2" valign="middle" id="credit" style='border-radius: 6px; border: 0; color: #8a8a8a; font-family: "Times New Roman", sans-serif; font-size: 12px; line-height: 150%; text-align: center; padding: 24px 0;' align="center">
+                                    <p style="margin: 0 0 16px;">Most Popular Tours in Bali – Hire Bali Driver<br>Powered by <a href="http://hirebalidriver.com/" style="color: #077944; font-weight: normal; text-decoration: underline;">Hire Bali Driver</a></p>
+                                 </td>
+                              </tr>
+                           </table>
+                        </td>
+                     </tr>
+                  </table>
+                  <!-- End Footer -->
+               </td>
+            </tr>
+         </table>
+      </div>
+   </body>
+</html>
---
+++ resources/views/emails/front/bookingcustomer.blade.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en-US">
+   <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+      <title>Most Popular Tours in Bali – Hire Bali Driver</title>
+   </head>
+   <body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0" style="padding: 0;">
+      <div id="wrapper" dir="ltr" style="background-color: #f7f7f7; margin: 0; padding: 70px 0; width: 100%; -webkit-text-size-adjust: none;" bgcolor="#f7f7f7" width="100%">
+         <table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%">
+            <tr>
+               <td align="center" valign="top">
+                  <div id="template_header_image">
+                     <p style="margin-top: 0;"><img src="https://hirebalidriver.com/wp-content/uploads/2022/08/logo-hbd-email.png" alt="Most Popular Tours in Bali – Hire Bali Driver" style="border: none; display: inline-block; font-size: 14px; font-weight: bold; height: auto; outline: none; text-decoration: none; text-transform: capitalize; vertical-align: middle; max-width: 100%; margin-left: 0; margin-right: 0;" border="0"></p>
+                  </div>
+                  <table border="0" cellpadding="0" cellspacing="0" width="600" id="template_container" style="background-color: #fff; border: 1px solid #dedede; box-shadow: 0 1px 4px rgba(0,0,0,.1); border-radius: 3px;" bgcolor="#fff">
+                     <tr>
+                        <td align="center" valign="top">
+                           <!-- Header -->
+                           <table border="0" cellpadding="0" cellspacing="0" width="100%" id="template_header" style='background-color: #077944; color: #fff; border-bottom: 0; font-weight: bold; line-height: 100%; vertical-align: middle; font-family: "Helvetica Neue",Helvetica,Roboto,Arial,sans-serif; border-radius: 3px 3px 0 0;' bgcolor="#077944">
+                              <tr>
+                                 <td id="header_wrapper" style="padding: 36px 48px; display: block;">
+                                    <h1 style='font-family: "Helvetica Neue",Helvetica,Roboto,Arial,sans-serif; font-size: 30px; font-weight: 300; line-height: 150%; margin: 0; text-align: left; text-shadow: 0 1px 0 #399469; color: #fff; background-color: inherit;' bgcolor="inherit">Thank you for your booking</h1>
+                                 </td>
+                              </tr>
+                           </table>
+                           <!-- End Header -->
+                        </td>
+                     </tr>
+                     <tr>
+                        <td align="center" valign="top">
+                           <!-- Body -->
+                           <table border="0" cellpadding="0" cellspacing="0" width="600" id="template_body">
+                              <tr>
+                                 <td valign="top" id="body_content" style="background-color: #fff;" bgcolor="#fff">
+                                    <!-- Content -->
+                                    <table border="0" cellpadding="20" cellspacing="0" width="100%">
+                                       <tr>
+                                          <td valign="top" style="padding: 48px 48px 32px;">
+                                             <div id="body_content_inner" style='color: #636363; font-family: "Helvetica Neue",Helvetica,Roboto,Arial,sans-serif; font-size: 14px; line-height: 150%; text-align: left;' align="left">
+                                                <p style="margin: 0 0 16px;">Hi Andre,</p>
+                                                <p style="margin: 0 0 16px;">Just to let you know — we've received your booking #11018, and it has been confirmed:</p>
+                                                <p style="margin: 0 0 16px;">Pay with cash on meeting.</p>
+                                                <h2 style='color: #077944; display: block; font-family: "Helvetica Neue",Helvetica,Roboto,Arial,sans-serif; font-size: 18px; font-weight: bold; line-height: 130%; margin: 0 0 18px; text-align: left;'>
+                                                   Order #11018
+                                                </h2>
+                                                <h2 style='color: #2f3134; display: block; font-family: "Helvetica Neue",Helvetica,Roboto,Arial,sans-serif; font-size: 18px; font-weight: bold; line-height: 130%; margin: 0 0 18px; text-align: left;'>
+                                                   Tour : Half Day Ubud Tour Ubud Unique Spots
+                                                </h2>
+                                                <p style='color: #2f3134; display: block; font-family: "Helvetica Neue",Helvetica,Roboto,Arial,sans-serif; font-size: 16px; line-height: 130%; margin: 0 0 18px; text-align: left;'>
+                                                    Option : Tour with Lunch
+                                                </p>
+                                                <p style='color: #2f3134; display: block; font-family: "Helvetica Neue",Helvetica,Roboto,Arial,sans-serif; font-size: 16px; line-height: 130%; margin: 0 0 18px; text-align: left;'>
+                                                   Travel Date : 01 January 2023
+                                                </p>
+                                                <div style="margin-bottom: 40px;">
+                                                   <table class="td" cellspacing="0" cellpadding="6" border="1" style="color: #636363; border: 1px solid #e5e5e5; vertical-align: middle; width: 100%; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif;" width="100%">
+                                                      <tbody>
+                                                         <tr class="order_item">
+                                                            <td class="td" style="color: #636363; border: 1px solid #e5e5e5; padding: 12px; text-align: left; vertical-align: middle; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif; word-wrap: break-word;" align="left">
+                                                               <span class="meta-label">Adult</span>
+                                                            </td>
+                                                            <td class="td" style="color: #636363; border: 1px solid #e5e5e5; padding: 12px; text-align: left; vertical-align: middle; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif; word-wrap: break-word;" align="left">
+                                                                <span class="meta-label">2 x USD 25</span>
+                                                             </td>
+                                                             <td class="td" style="color: #636363; border: 1px solid #e5e5e5; padding: 12px; text-align: left; vertical-align: middle; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif; word-wrap: break-word;" align="left">
+                                                                <span class="meta-label">USD 50</span>
+                                                             </td>
+                                                         </tr>
+                                                         <tr class="order_item">
+                                                            <td class="td" style="color: #636363; border: 1px solid #e5e5e5; padding: 12px; text-align: left; vertical-align: middle; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif; word-wrap: break-word;" align="left">
+                                                               <span class="meta-label">Child</span>
+                                                            </td>
+                                                            <td class="td" style="color: #636363; border: 1px solid #e5e5e5; padding: 12px; text-align: left; vertical-align: middle; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif; word-wrap: break-word;" align="left">
+                                                                <span class="meta-label">3 x USD 10</span>
+                                                             </td>
+                                                             <td class="td" style="color: #636363; border: 1px solid #e5e5e5; padding: 12px; text-align: left; vertical-align: middle; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif; word-wrap: break-word;" align="left">
+                                                                <span class="meta-label">USD 30</span>
+                                                             </td>
+                                                         </tr>
+                                                      </tbody>
+                                                      <tfoot>
+                                                         <tr>
+                                                            <th class="td" scope="row" colspan="2" style="color: #636363; border: 1px solid #e5e5e5; vertical-align: middle; padding: 12px; text-align: left; border-top-width: 4px;" align="left">Subtotal:</th>
+                                                            <td class="td" style="color: #636363; border: 1px solid #e5e5e5; vertical-align: middle; padding: 12px; text-align: left; border-top-width: 4px;" align="left">USD260,00</td>
+                                                         </tr>
+                                                         <tr>
+                                                            <th class="td" scope="row" colspan="2" style="color: #636363; border: 1px solid #e5e5e5; vertical-align: middle; padding: 12px; text-align: left;" align="left">Payment method:</th>
+                                                            <td class="td" style="color: #636363; border: 1px solid #e5e5e5; vertical-align: middle; padding: 12px; text-align: left;" align="left">Pay later (cash)</td>
+                                                         </tr>
+                                                         <tr>
+                                                            <th class="td" scope="row" colspan="2" style="color: #636363; border: 1px solid #e5e5e5; vertical-align: middle; padding: 12px; text-align: left;" align="left">Total:</th>
+                                                            <td class="td" style="color: #636363; border: 1px solid #e5e5e5; vertical-align: middle; padding: 12px; text-align: left;" align="left">USD260,00</td>
+                                                         </tr>
+                                                      </tfoot>
+                                                   </table>
+                                                </div>
+                                                <p style="margin: 0 0 16px;"><strong>Hotel:</strong> My Hotel</p>
+                                                <p style="margin: 0 0 16px;"><strong>Hotel address:</strong> Ubud</p>
+                                                <p style="margin: 0 0 16px;"><strong>Special requirements:</strong> Test order</p>
+                                                <table id="addresses" cellspacing="0" cellpadding="0" border="0" style="width: 100%; vertical-align: top; margin-bottom: 40px; padding: 0;" width="100%">
+                                                   <tr>
+                                                      <td valign="top" width="50%" style="text-align: left; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif; border: 0; padding: 0;" align="left">
+                                                         <h2 style='color: #077944; display: block; font-family: "Helvetica Neue",Helvetica,Roboto,Arial,sans-serif; font-size: 18px; font-weight: bold; line-height: 130%; margin: 0 0 18px; text-align: left;'>Billing address</h2>
+                                                         <address class="address" style="padding: 12px; color: #636363; border: 1px solid #e5e5e5;">
+                                                            Andre Nata
+                                                            <br><a href="tel:087860062474" style="color: #077944; font-weight: normal; text-decoration: underline;">087860062474</a>
+                                                            <br>andrenata25@gmail.com
+                                                            <br>Ubud Gianyar Bali
+                                                         </address>
+                                                      </td>
+                                                   </tr>
+                                                </table>
+                                                <p style="margin: 0 0 16px;">Thanks for using hirebalidriver.com! Please contact us on +6281933116036 prior to the activity date!</p>
+                                             </div>
+                                          </td>
+                                       </tr>
+                                    </table>
+                                    <!-- End Content -->
+                                 </td>
+                              </tr>
+                           </table>
+                           <!-- End Body -->
+                        </td>
+                     </tr>
+                  </table>
+               </td>
+            </tr>
+            <tr>
+               <td align="center" valign="top">
+                  <!-- Footer -->
+                  <table border="0" cellpadding="10" cellspacing="0" width="600" id="template_footer">
+                     <tr>
+                        <td valign="top" style="padding: 0; border-radius: 6px;">
+                           <table border="0" cellpadding="10" cellspacing="0" width="100%">
+                              <tr>
+                                 <td colspan="2" valign="middle" id="credit" style='border-radius: 6px; border: 0; color: #8a8a8a; font-family: "Helvetica Neue",Helvetica,Roboto,Arial,sans-serif; font-size: 12px; line-height: 150%; text-align: center; padding: 24px 0;' align="center">
+                                    <p style="margin: 0 0 16px;">Most Popular Tours in Bali – Hire Bali Driver<br>Powered by <a href="http://hirebalidriver.com/" style="color: #077944; font-weight: normal; text-decoration: underline;">Hire Bali Driver</a></p>
+                                 </td>
+                              </tr>
+                           </table>
+                        </td>
+                     </tr>
+                  </table>
+                  <!-- End Footer -->
+               </td>
+            </tr>
+         </table>
+      </div>
+   </body>
+</html>

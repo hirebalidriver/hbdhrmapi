bd3ff8baf7debc097683e1870f9c8283d6b50cc5
andrenata <andrenata25@gmail.com>
2023-10-27 09:00:36 +0800

    css button

--- resources/views/emails/guide/assignguide.blade.php
+++ resources/views/emails/guide/assignguide.blade.php
@@ -57,7 +57,16 @@
                                                 <p style="margin: 0 0 5px;"><strong>Payment:</strong> {{$details['status_payment']}}</p>
                                                 <p style="margin: 0 0 5px;"><strong>Collect:</strong> {{$details['collect']}}</p>
                                                 <p style="margin: 0 0 5px;"><strong>Special requirements:</strong> {{$details['note']}}</p>
-
+                                                
+                                                <div style="padding: 20 0 20 0;">   
+                                                    <a style="background-color: #4a4a4a;
+                                                    color: #fff;
+                                                    padding: 8px 20px;
+                                                    text-decoration:none;
+                                                    font-weight:bold;
+                                                    border-radius:5px;
+                                                    cursor:pointer;" href="https://guide.hirebalidriver.com/" target="_blank">Visit Guide Portal</a>
+                                                </div>
                                              </div>
                                           </td>
                                        </tr>

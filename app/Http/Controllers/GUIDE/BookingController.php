c82a835e5f9c6a59645a5f4769bb821a8cb29dfd
andrenata <andrenata25@gmail.com>
2023-11-01 10:01:51 +0800

    update

--- app/Http/Controllers/GUIDE/BookingController.php
+++ app/Http/Controllers/GUIDE/BookingController.php
@@ -40,8 +40,8 @@ class BookingController extends Controller
 
         $per_page = $request->input('per_page', 20);
         $page = $request->input('page', 1);
-        $sortBy = $request->sortBy == null ? $sortBy = 'date' : $sortBy = $request->sortBy;
-        $direction =$request->input('direction', 'DESC');
+        $sortBy = 'date';
+        $direction = 'ASC';
 
         if($request->date_from > $request->date_end){
             $start = $request->date_end;
